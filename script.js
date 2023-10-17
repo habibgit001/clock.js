1aa51af0a38149d59d8d196d7a504720ebd750d9
habibgit001 <146697808+habibgit001@users.noreply.github.com>
2023-10-17 19:45:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const getcolor = ()=>{
+    const randomNumber = Math.floor( Math.random()* 16777215);
+    const randomCode = "#"+randomNumber.toString(16);
+    document.body.style.backgroundColor = randomCode;
+    document.getElementById("colorcode").innerText = randomCode;
+
+    navigator.clipboard.writeText(randomCode)
+}
+
+document.getElementById("btn").addEventListener(
+    "click",
+    getcolor
+)
+
+
+
+
+
+getcolor();
